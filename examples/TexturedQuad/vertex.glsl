6f6c2190a2696b3b6a6a275c11909e21d2f8e01b
Stanislav Kirichenko - Housing Italy <stanislav@zuru.com>
2023-09-21 16:56:13 +0200

    texture quad finished

---
+++ examples/TexturedQuad/vertex.glsl
@@ -0,0 +1,16 @@
+#version 450
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec4 color;
+
+layout(location = 0) out vec2 pos;
+
+layout (binding =0) uniform MyMatrix
+{
+    mat4 Matrix;
+}ubo;
+
+void main() {
+    gl_Position = ubo.Matrix * vec4(position.xyz, 1.0);
+    pos = position.xy;
+}
